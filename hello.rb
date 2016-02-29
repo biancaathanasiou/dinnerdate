da96e9a56803a143aa16ed0af3c24182ffc8d84e
Bianca Athanasiou <biancaathanasiou@gmail.com>
2016-02-29 19:46:52 +0000

    sorting out files

--- hello.rb
+++ hello.rb
@@ -1,27 +1,45 @@
 require 'sinatra'
+require 'twitter'
 
 get('/') do
-  erb :welcomeguest
+  erb :hello
+end
+
+get('/twitter') do
+  twitter_api = Twitter::REST::Client.new do |config|
+	  config.consumer_key        = "qHLQaXq1TvWuU6tCYqhcfTP53"
+	  config.consumer_secret     = "E1027APLZnxFGTx39m6LDfbeT30WrCwTJT5R4Fs4RC8DN7Vmz4"
+	  config.access_token        = "29703751-8pefVB9fnoPBcTRnBuTomoUtuN2YE11vuXzwdAvtz"
+	  config.access_token_secret = "YBzKoqqvgA04FqIdCmLlNYspS0uw1I5X2mvFDYYFCBb3L"
+	end
+
+	cfg_tweets = twitter_api.search("CodeFirstGirls")
+
+	cfg_tweets.each do |tweet|
+	  puts "#{tweet.user.screen_name}: #{tweet.text}"
+	  erb :template
+
+
+	end
+
 end
 
 get('/:name') do
   @name = params[:name].capitalize
-  erb :welcomeguest
-end
-
-get('/:name/:time') do
-  @name = params[:name].capitalize
-  @time = params[:time]
-  erb :choosetime
+  erb :hello
 end
 
 
-post('/signup') do
+post('/venues') do
   puts params[:name]
   puts params[:email]
+  puts params[:occasion]
 
+
+ @email = params[:email]
+ @name = params[:name]
+ @occasion = params[:occasion].downcase
 erb :venues
 
 end
-
 
