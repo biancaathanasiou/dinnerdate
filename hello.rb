f5ebc9071c0d99a139e1004afcf48286b5301faa
Teaandmadness <catherine.j.heath@gmail.com>
2016-03-07 19:50:38 +0000

    Trying to add in Twitter API to submit button

--- hello.rb
+++ hello.rb
@@ -13,11 +13,12 @@ get('/twitter') do
 	  config.access_token        = "29703751-8pefVB9fnoPBcTRnBuTomoUtuN2YE11vuXzwdAvtz"
 	  config.access_token_secret = "YBzKoqqvgA04FqIdCmLlNYspS0uw1I5X2mvFDYYFCBb3L"
 	end
-
+twitter_api.update("i tweeted")
 	cfg_tweets = twitter_api.search("CodeFirstGirls")
 
 	cfg_tweets.each do |tweet|
 	  puts "#{tweet.user.screen_name}: #{tweet.text}"
+
 	  erb :template
 
 
@@ -100,6 +101,25 @@ puts params[:email]
 
 email = params[:email]
 @name = params[:name]
+
+
+#
+#   twitter_api = Twitter::REST::Client.new do |config|
+# 	  config.consumer_key        = "qHLQaXq1TvWuU6tCYqhcfTP53"
+# 	  config.consumer_secret     = "E1027APLZnxFGTx39m6LDfbeT30WrCwTJT5R4Fs4RC8DN7Vmz4"
+# 	  config.access_token        = "29703751-8pefVB9fnoPBcTRnBuTomoUtuN2YE11vuXzwdAvtz"
+# 	  config.access_token_secret = "YBzKoqqvgA04FqIdCmLlNYspS0uw1I5X2mvFDYYFCBb3L"
+# 	end
+# twitter_api.update("#{@name} has booked #{@location} for their #DinnerDate! Why not join them? dinnerdate.com")
+# 	cfg_tweets = twitter_api.search("CodeFirstGirls")
+
+# 	cfg_tweets.each do |tweet|
+# 	  puts "#{tweet.user.screen_name}: #{tweet.text}"
+
+# 	  erb :template
+
+
+# 	end
 
 
 Pony.options = {
